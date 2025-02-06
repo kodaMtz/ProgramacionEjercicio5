e82dafdb613b456171c390e07c562ea368b7f6cc
unknown <k2v3nkevoneto@gmail.com>
2025-02-06 14:42:18 -0600

    Ejercicio5

---
+++ src/js/Script.js
@@ -0,0 +1,18 @@
+
+/*
+Declara 3 variables para almacenar el nombre, edad y país de una persona.
+Luego, utiliza template strings(``) para crear una oración como:
+"Hola, mi nombre es [nombre], tengo [edad] años y soy de [país]."
+Muestra el resultado en consola.
+*/
+
+
+//Declaracion de variables 
+let nombre = "Kevin";
+let edad = 21;
+let ciudad = "Mexico";
+
+//Uso de temple String(``)
+console.log(`Hola, mi nombre es ${nombre}.
+     tengo ${edad} años y soy de
+     ${ciudad}, mucho gusto.`)
